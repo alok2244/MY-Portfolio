cd147755914c5970eba4d4891a18bd6412b11bc8
Alok Chauhan <alok.chauhan.256574242@gmai.com>
2024-04-14 20:16:23 +0530

    deply

---
+++ dist/Data/CareerData.js
@@ -0,0 +1,25 @@
+const CareerData = {
+    labels: ['Born', '10th', '12th', 'Computer Science Graduate', 'MCA', 'ASE in Accenture'],
+    datasets: [
+      {
+        label: 'Career Milestones',
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: 'rgba(75,192,192,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(75,192,192,1)',
+        data: [
+          '1999-01-01', // Date of birth
+          '2016-01-01', // 10th
+          '2018-01-01', // 12th
+          '2021-01-01', // Computer Science Graduate
+          '2023-01-01', // MCA
+          '2023-07-01'  // ASE in Accenture
+        ]
+      }
+    ]
+  };
+
+  export default CareerData
+  
